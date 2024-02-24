390ce946c30859b262853aa775d8348be97d5f16
Diogo Barros <diogofpbarros@gmail.com>
2024-02-24 16:09:26 +0000

    1st implementation of code blocks

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -8,6 +8,8 @@ import util.StringUtil;
 
 public class Parser {
 
+	private static final String CODE_BLOCK_DELIMITER = "```";
+	
 	public static void toHtml(Reader in, Writer out) throws IOException {
 		
 		StringBuilder sb = new StringBuilder();
@@ -25,6 +27,8 @@ public class Parser {
 				// parse
 				if (isHeading(line))
 					parseHeading(in, out, line);
+				else if (isCodeBlock(line))
+					parseCodeBlock(in, out, line);
 			}
 			else
 			{
@@ -46,5 +50,22 @@ public class Parser {
 	}
 	
 	
+	
+	private static boolean isCodeBlock(String line) {
+		return line.equals(CODE_BLOCK_DELIMITER);
+	}
+	
+	private static void parseCodeBlock(Reader in, Writer out, String firstLine) throws IOException {
+		// returns the language or empty string (because firstLine is always at least 3 in size)
+		String language = firstLine.substring(3);
+		
+		// collect all code until the delimiter
+		String code = "";
+		String line = "";
+		while ((line = StringUtil.readLine(in)) != CODE_BLOCK_DELIMITER)
+			code += line + "\n";
+		
+		out.write("<pre><code class=\"" + language + "\"> "+ code + "</code></pre>\n");
+	}
 	
 }
--- src/util/StringUtil.java
+++ src/util/StringUtil.java
@@ -1,8 +1,29 @@
 package util;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class StringUtil {
 
 	public static int countHeadingChars(String s, char c) {
 		return s.chars().takeWhile(i -> i == c).map(i -> 1).sum();
+	}
+	
+	public static String readLine(Reader in) {
+		StringBuilder sb = new StringBuilder();
+		int ascii;
+		
+		try 
+		{
+			while ((ascii = in.read()) >= 0) 
+				if (ascii == '\n') 
+					return sb.toString();
+				else
+					sb.append((char) ascii);
+		} catch (IOException e) {
+			return sb.toString();
+		}
+		
+		return sb.toString();
 	}
 }
